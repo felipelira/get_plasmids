dc77f3294a253e113ae6364c21b40f5f891aacb2
Felipe Lira <felipelira3@yahoo.com.br>
2018-08-21 11:26:03 +0200

    Update contigs2gbk.py

--- contigs2gbk.py
+++ contigs2gbk.py
@@ -7,13 +7,13 @@ import os
 import argparse
 from argparse import RawTextHelpFormatter
 
-#	contig2gbk.py [list] [dicrectory]
-
+#	contig2gbk.py -c [contig_ID] -g [genome.gbff] -o [output name]
+# 
 
 def get_arguments():
 	parser = argparse.ArgumentParser(prog = 'contig2gbk.py', usage= '\n\t%(prog)s [contig ID/ or list of contigs] [output prefix]' , description = "Split a .gbk (.gbff) file and retrieve the information of the contig of interest.",formatter_class=RawTextHelpFormatter)
 	parser.add_argument('-c', required = False, type = str, help='Single contig (e.g. NC_010697.1) or list of contigs to be extracted separated by commas (e.g. NC_010697.1,NC_010693.1).', metavar='')
-	parser.add_argument('-g', required = True, type = str, help='Genome file from whic contigs will be extracted', metavar='') # when using a single genome, -g is the name of genome file in .gbk (.gbff) format
+	parser.add_argument('-g', required = True, type = str, help='Genome file with contigs to be extracted', metavar='') # when using a single genome, -g is the name of genome file in .gbk (.gbff) format
 	parser.add_argument('-o', required = False, type = str, help='Name of output file', metavar='')
 	return parser
 
